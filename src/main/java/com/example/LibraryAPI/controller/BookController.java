d436754d7f563cc6ad9118e80c5f6a1e705e1b8e
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2025-08-14 10:47:52 -0400

    Update BookController.java

--- src/main/java/com/example/LibraryAPI/controller/BookController.java
+++ src/main/java/com/example/LibraryAPI/controller/BookController.java
@@ -1,13 +1,43 @@
 package com.example.LibraryAPI.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.LibraryAPI.dto.BookDto;
+import com.example.LibraryAPI.model.Book;
+import com.example.LibraryAPI.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
 
+    private final BookService bookService;
 
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/allBooks")
+    public List<Book> allBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/byTitle")
+    public Mono<String> getByTitle(@RequestParam String title) {
+        return bookService.getBookByTitle(title);
+    }
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        Book savedBook = bookService.addBook(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedBook);
+    }
+
+    @DeleteMapping("/deleteBook/{id}")
+    public void deleteBook(@PathVariable int id) {
+        bookService.deleteBook(id);
+    }
 }
