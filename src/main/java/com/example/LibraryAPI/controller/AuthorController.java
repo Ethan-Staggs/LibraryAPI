e2ba4d8a27e7870c0db6d98f3194bc71260f053b
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2025-08-14 10:45:38 -0400

    Initial commit

--- src/main/java/com/example/LibraryAPI/controller/AuthorController.java
+++ src/main/java/com/example/LibraryAPI/controller/AuthorController.java
@@ -1,11 +1,44 @@
 package com.example.LibraryAPI.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.LibraryAPI.model.Author;
+import com.example.LibraryAPI.repository.AuthorRepository;
+import com.example.LibraryAPI.service.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/author")
 public class AuthorController {
+
+    private final AuthorService authorService;
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/allAuthors")
+    public List<Author> getAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/byId")
+    public Author getAuthorById(@PathVariable int id) {
+        return authorService.getAuthorById(id);
+    }
+
+    @PostMapping("/addBook")
+    public ResponseEntity<Author> addAuthor(@RequestBody Author author) {
+        Author newAuthor = authorService.addAuthor(author);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newAuthor);
+    }
+
+    @DeleteMapping("/deleteAuthor/{id}")
+    public void deleteAuthor(@PathVariable int id) {
+        authorService.deleteAuthor(id);
+    }
+
 }
