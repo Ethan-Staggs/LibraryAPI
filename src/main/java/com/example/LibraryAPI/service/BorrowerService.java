d8a8cfcba7ae8d8df3768952e5c56ecee64dc656
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2025-08-14 10:48:55 -0400

    Long to int

--- src/main/java/com/example/LibraryAPI/service/BorrowerService.java
+++ src/main/java/com/example/LibraryAPI/service/BorrowerService.java
@@ -6,28 +6,37 @@ import com.example.LibraryAPI.model.Author;
 import com.example.LibraryAPI.model.Book;
 import com.example.LibraryAPI.model.Borrower;
 import com.example.LibraryAPI.model.Loan;
+import com.example.LibraryAPI.repository.BookRepository;
 import com.example.LibraryAPI.repository.BorrowerRepository;
+import com.example.LibraryAPI.repository.LoanRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class BorrowerService {
 
     private final BorrowerRepository borrowerRepository;
+    private final BookRepository bookRepository;
 
-    public BorrowerService(BorrowerRepository borrowerRepository) {
+    private final LoanRepository loanRepository;
+
+    public BorrowerService(BorrowerRepository borrowerRepository, BookRepository bookRepository, LoanRepository loanRepository) {
         this.borrowerRepository = borrowerRepository;
+        this.bookRepository = bookRepository;
+        this.loanRepository = loanRepository;
     }
 
     public List<Borrower> getAllBorrowers() {
         return borrowerRepository.findAll();
     }
 
-    public Borrower getBorrowerById(Long id) {
+    public Borrower getBorrowerById(int id) {
         return borrowerRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Borrower not found with id: " + id));
     }
@@ -43,7 +52,7 @@ public class BorrowerService {
     }
 
     public Borrower updateBorrower(BorrowerDto borrower) {
-        Borrower updatedBorrower = getBorrowerById(borrower.getId());
+        Borrower updatedBorrower = getBorrowerById(borrower.getId()); 
 
         updatedBorrower.setName(borrower.getName());
         updatedBorrower.setEmail(borrower.getEmail());
@@ -51,11 +60,36 @@ public class BorrowerService {
         return borrowerRepository.save(updatedBorrower);
     }
 
-    public void deleteBorrower(Long id) {
+    public void deleteBorrower(int id) {
         if (!borrowerRepository.existsById(id)) {
             throw new EntityNotFoundException("Book not found with id: " + id);
         }
 
         borrowerRepository.deleteById(id);
+    }
+
+    public void borrowBook(int borrowerId, int bookId) {
+        Borrower borrower = borrowerRepository.getReferenceById(borrowerId);
+        Loan loan = new Loan();
+        Book loanedBook = bookRepository.getReferenceById(bookId);
+        loan.setBorrower(borrower);
+        loan.setBook(loanedBook);
+        loan.setDueDate(LocalDate.now().plusDays(15));
+        loan.setReturned(false);
+        borrower.addLoan(loan);
+
+        loanRepository.save(loan);
+    }
+
+    public void returnBook(int borrowerId, int bookId, int loanId) {
+        Borrower borrower = borrowerRepository.getById(borrowerId);
+        Loan loan = loanRepository.getReferenceById(loanId);
+        Book book = bookRepository.getReferenceById(bookId);
+
+
+        borrower.removeLoan(loan);
+        loan.setReturned(true);
+
+
     }
 }
