de1fd5b21b31ab3c0f38bfef09a369c13084f77d
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2025-08-16 10:16:17 -0400

    Update Loan.java

--- src/main/java/com/example/LibraryAPI/model/Loan.java
+++ src/main/java/com/example/LibraryAPI/model/Loan.java
@@ -1,5 +1,6 @@
 package com.example.LibraryAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -10,7 +11,7 @@ import java.util.Date;
 public class Loan {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
 
     @Column(name = "id")
     private int id;
